2e1810bdb9c07055db92a3def11d26d6ca7affb4
Dacia Copeland <dacia.copeland@bison.howard.edu>
2018-11-16 12:10:32 -0500

    Begin Main Code
    Begin code using the system call for reading a string (8), then loading the address of that string into the $a0 register. I used the number 500(could be more or less) to give space for extra spaces in beginning, middle, and or end of string input.

--- project2.s
+++ project2.s
@@ -1,1 +1,10 @@
-
+#This program takes user's input, compares it to conditions, and prints out appropriate message/output
+.data
+	emptyInput: .ascizz "Input is empty." #message for empty string
+	longInput: .ascizz "Input it is too long." #messgae for string that has more than 4 characters
+	invalidInput: .ascizz "Invalid base-35 number." #message for string that includes one or more characters not in set
+.text
+	main:
+		li $v0, 8 #syscall to read string
+		la $a0, userInput #stores address of string
+		li $a1, 500 #create ample space for string input

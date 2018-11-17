36c92ff843320207e4083561cddb39dd3ed58116
daciacopeland99 <44177869+daciacopeland99@users.noreply.github.com>
2018-11-16 21:30:28 -0500

    Finish viewChars Branch
    Again set an increment for the counter to be able to loop through string, if equal to new line or null restart, if not equal to, continue loop.

--- project2.s
+++ project2.s
@@ -34,3 +34,7 @@
         viewChars:
             lb $t0, 0($t2)
             addi $t2, $t2, 1
+	    addi $t1, $t1, 1
+            beq $t0, 10, restart
+            beq $t0, 0, restart
+            bne $t0, 32, viewChars
